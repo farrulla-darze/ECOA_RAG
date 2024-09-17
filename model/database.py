1c5d1bca0538bccb2cb17b926cf8a580895d1134
Felipe Procaci Kestelman <fkestelman@inf.puc-rio.br>
2024-09-17 18:18:39 -0300

    Loaded database

--- model/database.py
+++ model/database.py
@@ -15,16 +15,6 @@ load_dotenv()
 
 openai_key = os.getenv("OPENAI_API_KEY")
 
-# urls = ["https://pt.wikipedia.org/wiki/Javier_Milei",
-#         "https://www.cbsnews.com/news/super-bowl-winners-list-history/",
-#         "https://ge.globo.com/futebol/times/corinthians/noticia/2024/02/16/corinthians-anuncia-a-contratacao-do-lateral-direito-matheuzinho.ghtml",
-#         "https://ge.globo.com/futebol/times/botafogo/noticia/2024/02/16/luiz-henrique-do-botafogo-tem-lesao-na-panturrilha.ghtml",
-#         "https://ge.globo.com/futebol/times/fluminense/noticia/2024/02/16/escalacao-do-fluminense-diniz-escala-reservas-contra-madureira-mas-tera-john-kennedy-e-douglas-costa.ghtml",
-#     "https://www.lemonde.fr/football/article/2024/02/25/ligue-1-le-psg-arrache-le-nul-de-justesse-contre-rennes-monaco-remonte-sur-le-podium_6218528_1616938.html",
-#     "https://ge.globo.com/futebol/futebol-internacional/futebol-frances/copa-da-franca/noticia/2024/02/27/perri-e-heroi-nos-penaltis-e-lyon-avanca-de-fase-na-copa-da-franca.ghtml",
-#     "https://ge.globo.com/tenis/noticia/2024/02/27/joao-fonseca-sofre-com-condicoes-ruins-da-quadra-no-atp-santiago-e-e-eliminado.ghtml",   
-# ]
-
 class Database(ABC):
     _instance = None
 
